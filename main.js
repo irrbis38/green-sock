27150ce434008e078cf961b9c71143aa319d19de
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-11-17 11:06:02 +0300

    feat: 0303 completed

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-window.addEventListener('DOMContentLoaded', () => {
-  startTween();
+window.addEventListener("DOMContentLoaded", () => {
+  // startTween();
+  startTimeline();
 });
 
+function startTimeline() {
+  const tl = gsap.timeline();
+  const duration = 1;
+  tl.to("#tools", duration, { y: -100, ease: Elastic.easeInOut });
+  tl.to("#power", duration, { y: -100 });
+
+  const startBtn = document.getElementById("start");
+  const pauseBtn = document.getElementById("pause");
+  const reverseBtn = document.getElementById("reverse");
+
+  startBtn.addEventListener("click", () => {
+    tl.play();
+  });
+  pauseBtn.addEventListener("click", () => {
+    tl.pause();
+  });
+  reverseBtn.addEventListener("click", () => {
+    tl.reverse();
+  });
+}
+
 function startTween() {
-  TweenLite.to('#tools', 1, {
+  TweenLite.to("#tools", 1, {
     x: 50,
     y: 100,
     scale: 1.5,
   });
 
-  TweenLite.to('.aspect', 1, { color: '#f33', fontSize: '+=50' });
+  TweenLite.to(".aspect", 1, { color: "#f33", fontSize: "+=50" });
 }
